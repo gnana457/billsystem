b4fabadd99bbeeb4e8e76c6930f5263e9fa126ce
gnana457 <gnanasekaran18457@gmail.com>
2018-07-14 17:31:52 +0530

    User Interface

--- src/app/addproduct/addproduct.component.ts
+++ src/app/addproduct/addproduct.component.ts
@@ -9,7 +9,7 @@ export class AddproductComponent implements OnInit {
   gst: SelectItem[];
   constructor() {
     this.gst = [
-      {label: '5%', value: {id: 1, value: 5}},
+      {label: '3%', value: {id: 1, value: 3}},
       {label: '12%', value: {id: 2, value: 12}},
       {label: '18%', value: {id: 3, value: 18}},
       {label: '28%', value: {id: 4, value: 28}},
